520603c93d07cb186f38d703bc59c8b1ccf4d6d6
Elijah Cleveland <elicleveland12@gmail.com>
2019-04-23 14:20:18 -0400

    create and display qr code with double tap

--- Frontend/screens/PetCard.js
+++ Frontend/screens/PetCard.js
@@ -3,12 +3,45 @@ import { ScrollView, StyleSheet, View, Text, SafeAreaView, TouchableOpacity, Lin
 import { ExpoLinksView } from '@expo/samples';
 import ToggleMissing from './ToggleMissing'
 import dateFormat from 'dateformat';
-
+import QRCode from 'react-native-qrcode';
 
 export default class PetCard extends React.Component {
 
+  state = {
+    qrOrImgToggle: false
+  }
+
+  lastTap = null
+
+  doubleTap = () => {
+    const now = Date.now()
+    const DOUBLE_PRESS_DELAY = 300
+    if (this.lastTap && (now - this.lastTap) < DOUBLE_PRESS_DELAY) {
+      this.setState({ qrOrImgToggle: !this.state.qrOrImgToggle })
+    } else {
+      this.lastTap = now
+    }
+  }
+
   renderInstagramButton = () => {
     if (this.props.selectedPet.instagram) {
+      if (this.state.qrOrImgToggle) {
+        return (<View style={{flex: 1, flexDirection: 'row', marginTop: 10, marginLeft: 10}}>
+            <QRCode
+            value={`retrievr-api.herokuapp.com/missing-pets ${this.props.selectedPet.id}`}
+            size={140}
+            bgColor='black'
+            fgColor='#00b894'
+            />
+          <TouchableOpacity style={{height:45, width: 45, marginLeft: 130, marginTop: -10}} onPress={()=> Linking.openURL(`http://instagram.com/${this.props.selectedPet.instagram}`)}>
+          <Image
+            source={{uri: `https://image.flaticon.com/icons/png/512/174/174855.png`}}
+            style={{height: 45, width: 45}}
+          />
+          </TouchableOpacity>
+        </View>
+      )
+    } else {
       return (<View style={{flex: 1, flexDirection: 'row'}}>
         <Image
           source={{uri: `${this.props.selectedPet.image}`}}
@@ -22,27 +55,40 @@ export default class PetCard extends React.Component {
         </TouchableOpacity>
       </View>
     )
+    }
   } else {
-    return (<View style={{flex: 1, flexDirection: 'row'}}>
-        <Image
-          source={{uri: `${this.props.selectedPet.image}`}}
-          style={styles.petImage}
-        />
-      </View>
-    )
-  }
+    if (this.state.qrOrImgToggle) {
+      return (<View style={{flex: 1, flexDirection: 'row', marginTop: 10, marginLeft: 10}}>
+          <QRCode
+          value={`retrievr-api.herokuapp.com/missing-pets ${this.props.selectedPet.id}`}
+          size={140}
+          bgColor='black'
+          fgColor='#00b894'
+          />
+        </View>
+        )
+      } else {
+        return (<View style={{flex: 1, flexDirection: 'row'}}>
+            <Image
+              source={{uri: `${this.props.selectedPet.image}`}}
+              style={styles.petImage}
+            />
+          </View>
+        )
+      }
+    }
   }
 
 
   render() {
     return (
-        <View style={styles.petCardContainer}>
+        <TouchableOpacity style={styles.petCardContainer} onPress={this.doubleTap}>
           <Text style={{color: 'white', paddingLeft: 10, paddingTop: 10, fontSize: 22}}>{this.props.selectedPet.name}</Text>
           {this.renderInstagramButton()}
           <Text style={{color: 'white', paddingLeft: 10, paddingTop: 5, fontSize: 16}}>{this.props.selectedPet.breed}</Text>
           <Text style={{color: 'white', paddingLeft: 10, paddingTop: 5, fontSize: 16}}>{this.props.selectedPet.age}</Text>
           <Text style={{color: 'white', paddingLeft: 10, paddingTop: 5, paddingBottom: 10, fontSize: 16}}>Next Vet Visit: {this.props.selectedPet.last_vet_visit ? dateFormat(this.props.selectedPet.last_vet_visit, "mmmm dS, yyyy") : "Not scheduled yet"}</Text>
-        </View>
+        </TouchableOpacity>
     );
   }
 }
