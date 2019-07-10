e007dff6d792db6377c3fd4c7b0e571b4a67a64a
github username <clarencekwong7@gmail.com>
2019-07-10 17:53:27 -0400

    added tag model and aws/email auth

--- Backendv2/db/schema.rb
+++ Backendv2/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_09_181756) do
+ActiveRecord::Schema.define(version: 2019_07_10_210210) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,12 @@ ActiveRecord::Schema.define(version: 2019_07_09_181756) do
     t.string "pet_description"
     t.string "poster_name"
     t.string "poster_phone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tags", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
+    t.boolean "active"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
